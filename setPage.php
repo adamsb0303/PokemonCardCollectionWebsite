f7e294371cf036ecbd945f87733723ee27e56931
Ben Adams <adamsb32578@gmail.com>
2021-09-04 12:22:43 -0400

    master set size and total price is in header

--- setPage.php
+++ setPage.php
@@ -2,148 +2,130 @@
 <html>
     <head>
         <link rel="stylesheet" href="CSS/setPage.css">
+        <?php
+            $name = $_GET['set'];
+            echo '<title>' . $name . '</title>';
+        ?>
     </head>
     <body>
-        <div class="header">
-            <?php
-                include_once 'php/connect.php';
-                $name = $_GET['set'];
+        <?php
+            include_once 'php/connect.php';
+            $sql = "SELECT * FROM `card` INNER JOIN `variant` ON card.variant_id = variant.variant_id
+                    WHERE `set_id` = (SELECT `set_id` FROM `set` WHERE set_name = '$name')";
 
+            $variant_result = mysqli_query($link, $sql) or die(mysqli_error($link));
+
+            $cardAmount = 0;
+            $marketPriceSum = 0;
+            $averagePriceSum = 0;
+
+            $cardTable = '<table>' .
+                '<th>' .
+                    'Set Num' .
+                '</th>' .
+                '<th style="text-align:left;">' .
+                    'Name' .
+                '</th>' .
+                '<th>' .
+                    'Market Price' .
+                '</th>' .
+                '<th>' .
+                    'Average Price' .
+                '</th>' .
+                '<th>' .
+                    'Price Purchased' .
+                '</th>' .
+                '<th>' .
+                    'Date Purchased' .
+                '</th>' .
+                '<th>' .
+                    'Condition' .
+                '</th>' .
+                '<th>' .
+                    '# in Inventory' .
+                '</th>';
+            while($variant = mysqli_fetch_array($variant_result)){
+                $cardAmount++;
+                if($variant['variant_name'] !== 'Normal'){
+                    $cardTable .= '<tr class="variantRow">' .
+                        //Set Num
+                        '<td>' .
+                        '</td>' .
+                        //Card Name
+                        '<td>' .
+                            '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;∟&nbsp;&nbsp;' . $variant['variant_name'] .
+                        '</td>';
+                } else{
+                    $cardTable .= '<tr class="cardRow">' .
+                        //Set Num
+                        '<td>' .
+                            $variant['set_num'] .
+                        '</td>' .
+                        //Card Name
+                        '<td>' .
+                            '<a href="https://www.tcgplayer.com/product/' . $variant['product_id'] . '">' . $variant['card_name'] . '</a>' .
+                        '</td>';
+                }
+                //Market Price
+                $cardTable .= '<td>';
+                    if($variant['market_price'] != 0){
+                        $cardTable .= '<text id="MP_' . $variant['card_id'] . '">$' . $variant['market_price'] . '</text>';
+                        $marketPriceSum += $variant['market_price'];
+                    }else
+                        $cardTable .= '<text id="MP_' . $variant['card_id'] . '">-</text>';
+                $cardTable .= '</td>' .
+                //Average Price
+                '<td>';
+                    if($variant['average_price'] != 0){
+                        $cardTable .= '<text id="AV_' . $variant['card_id'] . '">$' . $variant['average_price'] . '</text>';
+                        $averagePriceSum += $variant['average_price'];
+                    }else
+                        $cardTable .= '<text id="AV_' . $variant['card_id'] . '">-</text>'; 
+                $cardTable .= '</td>' .
+                //Price Purchased
+                '<td id="Price Purchased">' .
+                    '<input id="PP_' . $variant['card_id'] . '" type="number" min="0" step="0.01" placeholder = "$0.00"/>' .
+                    '<button title="Calculates price based off potential pulls" type="button">From Pack</button>' .
+                '</td>' .
+                //Date Purchased
+                '<td id="Date Purchased">' .
+                    '<input id="DP_' . $variant['card_id'] . '" type="date"></input>' .
+                '</td>' .
+                //Condition
+                '<td id="Date Purchased">' .
+                    '<select id="CND_' . $variant['card_id'] . '">' .
+                        '<option value="Near Mint">NM</option>' .
+                        '<option value="Light Played">LP</option>' .
+                        '<option value="Medium Played">MP</option>' .
+                        '<option value="Heavy Played">HP</option>' .
+                        '<option value="Damaged">DMG</option>' .
+                    '</select>' .
+                '</td>' .
+                //# in Inventory
+                '<td id="Number in Inventory">' .
+                    '<text id="INV_' . $variant['card_id'] . '">0</text>' .
+                '</td>' .
+                //Submit Button
+                '<td>' .
+                    '<button type="submit" onclick="addToInventory(' . $variant['card_id'] . ')">Add to Inventory</button>' .
+                '</td>' .
+            '</tr>';
+            }
+            $cardTable .= '</table>';
+
+            echo '<div class="header">';
                 $sql = "SELECT * FROM `set` WHERE set_name = '$name'";
                 $result = mysqli_query($link, $sql) or die(mysqli_error($link));
                 $row = mysqli_fetch_array($result);
                 echo '<img src="Images/Logos/' . $row['set_name'] . '.png" alt="' . $row['set_name'] . ' Set Symbol" style="width:25vw; height:auto;">' . '<br />';
                 echo $name . '<br />';
                 echo "Set Size: " . $row['set_size'] . '<br />';
+                echo "Master Set Size: " . $cardAmount . '<br />';
+                echo "Master Set Market Price: $" . number_format($marketPriceSum, 2) . '<br />';
                 echo "Progress: ";
-            ?>
-        </div>
+            echo '</div><br/>';
 
-        <br />
-
-        <?php
-            $name = $_GET['set'];
-
-            $sql = "SELECT * FROM `card` INNER JOIN `variant` ON card.variant_id = variant.variant_id
-                    WHERE `set_id` = (SELECT `set_id` FROM `set` WHERE set_name = '$name')";
-
-            $variant_result = mysqli_query($link, $sql) or die(mysqli_error($link));
-
-            $marketPriceSum = 0;
-            $averagePriceSum = 0;
-
-            echo '<table>';
-                echo '<th>';
-                    echo 'Set Num';
-                echo '</th>';
-                echo '<th style="text-align:left;">';
-                    echo 'Name';
-                echo '</th>';
-                echo '<th>';
-                    echo 'Market Price';
-                echo '</th>';
-                echo '<th>';
-                    echo 'Average Price';
-                echo '</th>';
-                echo '<th>';
-                    echo 'Price Purchased';
-                echo '</th>';
-                echo '<th>';
-                    echo 'Date Purchased';
-                echo '</th>';
-                echo '<th>';
-                    echo 'Condition';
-                echo '</th>';
-                echo '<th>';
-                    echo '# in Inventory';
-                echo '</th>';
-            while($variant = mysqli_fetch_array($variant_result)){
-                if($variant['variant_name'] !== 'Normal'){
-                    echo '<tr class="variantRow">';
-                        //Set Num
-                        echo '<td>';
-                        echo '</td>';
-                        //Card Name
-                        echo '<td>';
-                            echo '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;∟&nbsp;&nbsp;' . $variant['variant_name'];
-                        echo '</td>';
-                } else{
-                    echo '<tr class="cardRow">';
-                        //Set Num
-                        echo '<td>';
-                            echo $variant['set_num'];
-                        echo '</td>';
-                        //Card Name
-                        echo '<td>';
-                            echo '<a href="https://www.tcgplayer.com/product/' . $variant['product_id'] . '">' . $variant['card_name'] . '</a>';
-                        echo '</td>';
-                }
-                //Market Price
-                echo '<td>';
-                    if($variant['market_price'] != 0){
-                        echo '<text id="MP_' . $variant['card_id'] . '">$' . $variant['market_price'] . '</text>';
-                        $marketPriceSum += $variant['market_price'];
-                    }else
-                        echo '<text id="MP_' . $variant['card_id'] . '">-</text>';
-                echo '</td>';
-                //Average Price
-                echo '<td>';
-                    if($variant['average_price'] != 0){
-                        echo '<text id="AV_' . $variant['card_id'] . '">$' . $variant['average_price'] . '</text>';
-                        $averagePriceSum += $variant['average_price'];
-                    }else
-                        echo '<text id="AV_' . $variant['card_id'] . '">-</text>'; 
-                echo '</td>';
-                //Price Purchased
-                echo '<td id="Price Purchased">';
-                    echo '<input id="PP_' . $variant['card_id'] . '" type="number" min="0" step="0.01" placeholder = "$0.00"/>';
-                    echo '<button title="Calculates price based off potential pulls" type="button">From Pack</button>';
-                echo '</td>';
-                //Date Purchased
-                echo '<td id="Date Purchased">';
-                    echo '<input id="DP_' . $variant['card_id'] . '" type="date"></input>';
-                echo '</td>';
-                //Condition
-                echo '<td id="Date Purchased">';
-                    echo '<select id="CND_' . $variant['card_id'] . '">';
-                        echo '<option value="Near Mint">NM</option>';
-                        echo '<option value="Light Played">LP</option>';
-                        echo '<option value="Medium Played">MP</option>';
-                        echo '<option value="Heavy Played">HP</option>';
-                        echo '<option value="Damaged">DMG</option>';
-                    echo '</select>';
-                echo '</td>';
-                //# in Inventory
-                echo '<td id="Number in Inventory">';
-                    echo '<text id="INV_' . $variant['card_id'] . '">0</text>';
-                echo '</td>';
-                //Submit Button
-                echo '<td>';
-                    echo '<button type="submit" onclick="addToInventory(' . $variant['card_id'] . ')">Add to Inventory</button>';
-                echo '</td>';
-            echo '</tr>';
-            }
-            echo '<tr class="cardRow">';
-                echo '<td>';
-                echo '</td>';
-                echo '<td style="text-align:left;">';
-                echo '</td>';
-                echo '<td>';
-                    echo "$" . number_format($marketPriceSum, 2);
-                echo '</td>';
-                echo '<td>';
-                    echo "$" . number_format($averagePriceSum, 2);
-                echo '</td>';
-                echo '<td>';
-                echo '</td>';
-                echo '<td>';
-                echo '</td>';
-                echo '<td>';
-                echo '</td>';
-                echo '<td>';
-                echo '</td>';
-            echo '</tr>';
-            echo '</table>';
+            echo $cardTable;
         ?>
     </body>
 </html>
