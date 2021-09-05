527dad378bbaba301d0f3c26a25723ebc8a41b97
Ben Adams <adamsb32578@gmail.com>
2021-09-05 14:14:45 -0400

    updates set and master set price

--- php/update_card_prices.php
+++ php/update_card_prices.php
@@ -5,18 +5,30 @@
     include_once "bearer_token.php";
     set_time_limit(60000);
 
-    $sql = "SELECT * FROM `card` GROUP BY `product_id`";
-    $result = mysqli_query($link, $sql) or die(mysqli_error($link));
+    $sql = "SELECT `set_id` FROM `set`";
+    $sets = mysqli_query($link, $sql) or die(mysqli_error($link));
 
     $index = 1;
-    while($row = mysqli_fetch_array($result)){
-        if($row['product_id'] != 0){
-            updateCardPrice($row['product_id'], $bearerToken);
+    while($setRow = mysqli_fetch_array($sets)){
+        $setNum = $setRow['set_id'];
+        $sql = "SELECT * FROM `card` WHERE `set_id` = $setNum
+                GROUP BY `product_id`";
+        $products = mysqli_query($link, $sql) or die(mysqli_error($link));
+
+        while($productRow = mysqli_fetch_array($products)){
+            if($productRow['product_id'] != 0){
+                updateCardPrice($productRow['product_id'], $bearerToken);
+            }
+            if($index % 300 == 0){
+                sleep(60);
+            }  
+            $index++;
         }
-        if($index % 300 == 0){
-            sleep(60);
-        }  
-        $index++;
+
+        $sql = "UPDATE `set` SET `set_price` = (SELECT SUM(`market_price`) FROM `card` WHERE `set_id` = $setNum AND `variant_id` = 1),
+                                `Mset_price` = (SELECT SUM(`market_price`) FROM `card` WHERE `set_id` = $setNum)
+                WHERE `set_id` = $setNum";
+        mysqli_query($link, $sql) or die(mysqli_error($link));
     }
 
     function updateCardPrice($product_id, $bearerToken){
@@ -28,6 +40,7 @@
     
         $firstCardRow = mysqli_fetch_array($result);
         $cardIDIndex = $firstCardRow['card_id'];
+
         for($i = 0; $i < count($productPrices); $i++){
             if($productPrices[$i]->marketPrice != null){
                 $marketprice = $productPrices[$i]->marketPrice;
@@ -37,8 +50,7 @@
                 $marketaverage = ($marketaverage == null) ? 0 : $marketaverage;
 
                 $sql = "UPDATE `card` SET `market_price` = $marketprice, `average_price` = $marketaverage WHERE `card_id` = $cardIDIndex";
-                echo $sql . '<br/>';
-                $result = mysqli_query($link, $sql) or die(mysqli_error($link));
+                mysqli_query($link, $sql) or die(mysqli_error($link));
 
                 $cardIDIndex++;
             }
