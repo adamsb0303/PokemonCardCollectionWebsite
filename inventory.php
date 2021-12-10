aaa8640923c55e1d9dee3b24f058c740cbf5a8e8
Ben Adams <adamsb32578@gmail.com>
2021-12-10 14:24:39 -0500

    styled inventory page

--- inventory.php
+++ inventory.php
@@ -16,45 +16,45 @@
             include 'php/header.php';
             include_once 'php/connect.php';
 
-            $totalGain = 0;
-            $invTable = '';
-
             $sql = "SELECT * FROM `collection`
                     JOIN `card` ON `collection`.`card_id` = `card`.`card_id`
                     JOIN `set` ON `card`.`set_id` = `set`.`set_id`
                     JOIN `variant` ON `variant`.`variant_id` = `card`.`variant_id`
                     ORDER BY `card`.`set_id`, `card`.`card_id`";
             $result = mysqli_query($link, $sql) or die(mysqli_error($link));
-            $invTable .= '<table>' .
-                '<th>' .
-                    'Card Name' .
-                '</th>' .
-                '<th>' .
-                    'Variant' .
-                '</th>' .
-                '<th>' .
-                    'Set' .
-                '</th>' .
-                '<th>' .
-                    'Set #' .
-                '</th>' .
-                '<th>' .
-                    'Current Market Price' .
-                '</th>' .
-                '<th>' .
-                    'Puchase Price' .
-                '</th>' .
-                '<th>' .
-                    'Purchase Date' .
-                '</th>' .
-                '<th>' .
-                    'Condition' .
-                '</th>' .
-                '<th>' .
-                    'Current Gain' .
-                '</th>';
+        ?>
+        <div class="root">
+            <table>
+                <th>
+                    Card Name
+                </th>
+                <th>
+                    Variant
+                </th>
+                <th>
+                    Set
+                </th>
+                <th>
+                    Set #
+                </th>
+                <th>
+                    Current Market Price
+                </th>
+                <th>
+                    Puchase Price
+                </th>
+                <th>
+                    Purchase Date
+                </th>
+                <th>
+                    Condition
+                </th>
+                <th>
+                    Current Gain
+                </th>
+        <?php
             while($row = mysqli_fetch_array($result)){
-                $invTable .= '<tr>' .
+                echo '<tr>' .
                     //Card Name
                     '<td>' .
                         $row['card_name'] .
@@ -78,40 +78,37 @@
                     //Purchase Price
                     '<td>';
                         if($row['purchase_price'] != '')
-                            $invTable .= '$' . $row['purchase_price'];
+                            echo '$' . $row['purchase_price'];
                         else
-                            $invTable .= '-';
-                    $invTable .= '</td>' .
+                            echo '-';
+                    echo '</td>' .
                     //Purchase Date
                     '<td>';
                         if($row['purchase_date'] != '')
-                            $invTable .= $row['purchase_date'];
+                            echo $row['purchase_date'];
                         else
-                            $invTable .= '-';
-                    $invTable .= '</td>' .
+                            echo '-';
+                    echo '</td>' .
                     '</td>' .
                     //Condition
                     '<td>';
                         if($row['condition'] != '')
-                            $invTable .= $row['condition'];
+                            echo $row['condition'];
                         else
-                            $invTable .= '-';
-                    $invTable .= '</td>' .
+                            echo '-';
+                    echo '</td>' .
                     '</td>' .
                     //Current Gain
                     '<td>';
                         if($row['purchase_price'] != ""){
-                            $totalGain += $row['market_price'] - $row['purchase_price'];
-                            $invTable .= '$' . $row['market_price'] - $row['purchase_price'];
+                            echo '$' . number_format($row['market_price'] - $row['purchase_price'],2);
                         }else
-                            $invTable .= '-';
+                            echo '-';
                     '</td>' .
                 '</tr>';
             }
-            $invTable .= '</table>';
-
-            echo '$' . number_format($totalGain, 2);
-            echo $invTable;
+            echo '</table>';
         ?>
+        </div>
     </body>
 </html>
