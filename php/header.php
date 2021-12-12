af12c46b16b47622dffe7f70cbcbef2f4128355c
adamsb0303 <adamsb32578@gmail.com>
2021-12-12 01:11:20 +0000

    added search to the header

--- php/header.php
+++ php/header.php
@@ -5,6 +5,7 @@
                 <a class="navItem" href="./index.php">Home</a>
                 <a class="navItem" href="./setList.php">Card Sets</a>
                 <a class="navItem" href="./inventory.php">Inventory</a>
+                <a class="navItem" href="./search.php">Search</a>
             </nav>
         </div>
         <div class="siteLinks">
