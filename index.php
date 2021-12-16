9c62f45d84b3d0fc42b38a74b625e7180599a4f7
adamsb0303 <adamsb32578@gmail.com>
2021-12-16 23:29:33 +0000

    displays user profit on home page

--- index.php
+++ index.php
@@ -8,7 +8,18 @@
         <?php
             include "php/header.php";
 
-            $sql = "SELECT user_value, user_cost FROM user";
+            if($signedIn){
+                $sql = "SELECT user_value, user_cost FROM user
+                        JOIN collection ON collection.user_id = user.user_id
+                        WHERE `collection`.`user_id` = '" . $_COOKIE["ID"] . "'
+                        AND `user_key` = '" . $_COOKIE["Key"] . "'";
+                $result = mysqli_query($link, $sql) or die(mysqli_error($link));
+                $user_values = mysqli_fetch_array($result);
+
+                echo "Inventory Value: $" . $user_values[0] . "<br/>";
+                echo "Inventory Cost: $" . $user_values[1] . "<br/>";
+                echo "Total Profit: $" . ($user_values[0] - $user_values[1]);
+            }
         ?>
     </body>
 </html>
