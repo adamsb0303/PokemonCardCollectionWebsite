c5800f91689650f4339a8fae1730e6e09440c08c
adamsb0303 <adamsb32578@gmail.com>
2021-12-16 22:28:00 +0000

    underline links

--- index.php
+++ index.php
@@ -7,17 +7,6 @@
     <body>
         <?php
             include "php/header.php";
-            if(count($_COOKIE) > 0) {
-              echo "Cookies are enabled.\n";
-            } else {
-              echo "Cookies are disabled.\n";
-            }
-            if(!isset($_COOKIE["ID"])) {
-                echo "Cookie named 'ID' is not set!";
-              } else {
-                echo "Cookie 'ID' is set!<br>";
-                echo "ID is: " . $_COOKIE['ID'];
-            }
         ?>
     </body>
 </html>
